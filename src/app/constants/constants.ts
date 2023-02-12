082b09ecb453b6eb36815313f8272c3c097b090f
osu5nc <osu5nc@gmail.com>
2023-02-11 20:36:12 -0500

    Cleanup

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,7 @@
+export const constants = {
+    scarletPrice: 780,
+    grayPrice: 475,
+    singlePrice: 175,
+    holeSponsorPrice: 150,
+    lunchPrice: 40
+}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { constants } from '../constants/constants';
 
 @Component({
   selector: 'app-registration',
@@ -16,38 +17,46 @@ export class RegistrationComponent {
   totalRaffleTickets: number = 0;
   extraLunch: boolean = false;
   extraRaffle: boolean = false;
-  sponsorOnly: boolean = false;
+  skippingGolf: boolean = false;
+  GOLF = constants;
 
   public calculateTotalCost(): void {
     const participation = (<any>document.forms)['golfSignup'].elements['participation'].value;
     if (participation == 'scarlet') {
-      this.totalCost = 780;
+      this.totalCost = this.GOLF.scarletPrice;
       this.totalLunches = 4;
       this.totalGolfers = 4;
       this.holeSponsor = true;
       this.totalRaffleTickets = 12;
-      this.sponsorOnly = false;
+      this.skippingGolf = false;
     } else if (participation == 'gray') {
-      this.totalCost = 475;
+      this.totalCost = this.GOLF.grayPrice;
       this.totalLunches = 2;
       this.totalGolfers = 2;
       this.holeSponsor = true;
       this.totalRaffleTickets = 6;
-      this.sponsorOnly = false;
+      this.skippingGolf = false;
     } else if (participation == 'single') {
-      this.totalCost = 175;
+      this.totalCost = this.GOLF.singlePrice;
       this.totalLunches = 1;
       this.totalGolfers = 1;
       this.holeSponsor = false;
       this.totalRaffleTickets = 3;
-      this.sponsorOnly = false;
+      this.skippingGolf = false;
     } else if (participation == 'holeSponsor') {
-      this.totalCost = 150;
+      this.totalCost = this.GOLF.holeSponsorPrice;
       this.totalLunches = 1;
       this.totalGolfers = 0;
       this.holeSponsor = true;
       this.totalRaffleTickets = 0;
-      this.sponsorOnly = true;
+      this.skippingGolf = true;
+    } else if (participation == 'lunchOnly') {
+      this.totalCost = this.GOLF.lunchPrice;
+      this.totalLunches = 1;
+      this.totalGolfers = 0;
+      this.holeSponsor = false;
+      this.totalRaffleTickets = 0;
+      this.skippingGolf = true;
     }
     else {
       this.totalCost = 0;
@@ -57,17 +66,17 @@ export class RegistrationComponent {
       this.totalRaffleTickets = 0;
     }
     if(this.extraLunch || this.holeSponsor) {
-      if((<HTMLInputElement>document.getElementById('lunch1')) && (<HTMLInputElement>document.getElementById('lunch1')).value && !this.sponsorOnly) {
-        this.totalCost += 40;
+      if((<HTMLInputElement>document.getElementById('lunch1')) && (<HTMLInputElement>document.getElementById('lunch1')).value) {
+        this.totalCost += this.GOLF.lunchPrice;
       }
       if((<HTMLInputElement>document.getElementById('lunch2')) && (<HTMLInputElement>document.getElementById('lunch2')).value) {
-        this.totalCost += 40;
+        this.totalCost += this.GOLF.lunchPrice;
       }
       if((<HTMLInputElement>document.getElementById('lunch3')) && (<HTMLInputElement>document.getElementById('lunch3')).value) {
-        this.totalCost += 40;
+        this.totalCost += this.GOLF.lunchPrice;
       }
       if((<HTMLInputElement>document.getElementById('lunch4')) && (<HTMLInputElement>document.getElementById('lunch4')).value) {
-        this.totalCost += 40;
+        this.totalCost += this.GOLF.lunchPrice;
       }
     }
     if((<HTMLInputElement>document.getElementById('raffle1')) && (<HTMLInputElement>document.getElementById('raffle1')).value) {
